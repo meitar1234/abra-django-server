a8d5877bc9ec08a71b556ae9ceca7801b95e138c
bar oved <baro@silvernet.co.il>
2022-11-23 02:58:24 +0200

    some changes15

--- project/project/settings.py
+++ project/project/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n$us&q!r=l7va4_k86+a4ys!q81ik%^$@i=oxok5g21d*xc)^t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['abra-django-server.herokuapp.com']
 
 # Application definition
 
